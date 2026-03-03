04c412fc37e5e0d570152e4d9aed52d43ad07a01
M Aadil Rajputecho <maadilrajput05@gmail.com>
2026-03-03 12:36:07 +0500

    feature -2 update

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const server = http.createServer((req, res) => {
   res.end();
 });
 
+
+// console.log("main branch");
+
+// console.log("Feature 1 code");
+console.log("Feature  code");
+
+
 server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
